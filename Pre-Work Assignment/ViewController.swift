f98e40c765bffb8216209adf228d561e02c1abea
Zach Arnold <arnoldza@msu.edu>
2020-09-15 19:34:56 -0400

    Initial Commit

---
+++ Pre-Work Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pre-Work Assignment
+//
+//  Created by Zach Arnold on 9/15/20.
+//  Copyright © 2020 Zach Arnold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
