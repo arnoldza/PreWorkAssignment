e4083e25839b4b938855297908a774afcdeafaae
Zach Arnold <arnoldza@msu.edu>
2020-09-21 23:33:42 -0400

    More changes made, making settings page was attempted but broke code, so wasn't included in commit.

--- Pre-Work Assignment/ViewController.swift
+++ Pre-Work Assignment/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipAmountSegmentedControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
     }
+    @IBAction func onTap(_ sender: Any) {
+    }
+    
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipAmountSegmentedControl.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
